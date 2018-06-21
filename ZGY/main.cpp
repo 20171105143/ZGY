3d7075d63a380d5ade07b394e5e5707063198b98
zgy <s20171105143@mac-20.local>
2018-06-21 09:22:55 +0800

    Initial Commit

---
+++ ZGY/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ZGY
+//
+//  Created by zgy on 18/6/21.
+//  Copyright © 2018年 zgy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
