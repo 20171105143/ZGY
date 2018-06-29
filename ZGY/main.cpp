8241e9845efa62307d135b6e17a9f95a78d42e27
20171105143 <2468900412@qq.com>
2018-06-29 20:49:45 +0800

    6.29 8.48
    6.29.1

--- ZGY/main.cpp
+++ ZGY/main.cpp
@@ -1,15 +1,29 @@
-//
-//  main.cpp
-//  ZGY
-//
-//  Created by zgy on 18/6/21.
-//  Copyright © 2018年 zgy. All rights reserved.
-//
-
+#include <fstream>
+#include <string>
 #include <iostream>
+#include <streambuf>
 using namespace std;
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+int main()
+{
+    //定义文件输出流
+    ofstream oFile;
+    
+    //打开要输出的文件
+    oFile.open("scoresheet.csv", ios::out | ios::trunc);    // 这样就很容易的输出一个需要的excel 文件
+    oFile << "姓名" << "," << "年龄" << "," << "班级" << "," << "班主任" << endl;
+    oFile << "张三" << "," << "22" << "," << "1" << "," << "JIM" << endl;
+    oFile << "李四" << "," << "23" << "," << "3" << "," << "TOM" << endl;
+    
+    oFile.close();
+    
+    
+    //打开要输出的文件
+    ifstream iFile("scoresheet.csv");
+    string   readStr((std::istreambuf_iterator<char>(iFile)),  std::istreambuf_iterator<char>());
+    cout <<  readStr.c_str();
+    
     return 0;
-}
+}  
+
+
